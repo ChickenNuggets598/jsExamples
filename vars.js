ea5e2732ff68fae13ee484f14de8ffcf9d8ca63a
Kristy Shinoda <kshinoda@Kristys-MacBook-Pro.local>
2024-10-13 11:31:05 -0700

    add bool

--- vars.js
+++ vars.js
@@ -1,8 +1,10 @@
 // data types: numbers, string, boolean, array, JSON, functions
 let n = 1234;
 let str = "aaaa";
+let b = true;
 
 
 // variable types: var, let, const
 console.log('Number example: ' + n);
 console.log('String ex: ' + str);
+console.log('Boolean ex: ' + b);
