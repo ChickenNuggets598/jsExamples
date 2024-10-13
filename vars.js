c1afee2bcfeff14de1f011e5ea7c87a0f09330fc
Kristy Shinoda <kshinoda@Kristys-MacBook-Pro.local>
2024-10-13 11:29:33 -0700

    add vars

---
+++ vars.js
@@ -0,0 +1,8 @@
+// data types: numbers, string, boolean, array, JSON, functions
+let n = 1234;
+let str = "aaaa";
+
+
+// variable types: var, let, const
+console.log('Number example: ' + n);
+console.log('String ex: ' + str);
